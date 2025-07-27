8afaf0bb58223adb8d46bd0973e5332b8c0dee0b
lczm <zemingchua@gmail.com>
2025-07-27 18:24:45 +0800

    day21 part2
    thanks... used someones greedy approach

--- 21.rb
+++ 21.rb
@@ -1,144 +1,161 @@
-filename = "21.input"
 filename = "21.sample"
+filename = "21.input"
 
 lines = File.readlines(filename, chomp:true)
 p lines
 
-numeric_keypad =   
-  ['7','8','9'],
-  ['4','5','6'],
-  ['1','2','3'],
-  [' ','0','A']
-
-directional_keypad =
-  [' ','^','A'],
-  ['<','v','>']
-
-NUMERIC_POSITIONS = {}
-numeric_keypad.each_with_index do |row, i|
-  row.each_with_index do |val, j|
-    NUMERIC_POSITIONS[val] = [i, j] unless val == ' '
-  end
-end
-
-DIRECTIONAL_POSITIONS = {}
-directional_keypad.each_with_index do |row, i|
-  row.each_with_index do |val, j|
-    DIRECTIONAL_POSITIONS[val] = [i, j] unless val == ' '
-  end
-end
-
-DELTAS = {
-  [-1,  0] => '^',
-  [ 1,  0] => 'v',
-  [ 0, -1] => '<',
-  [ 0,  1] => '>' 
+NUMERIC_POSITIONS = {
+  '7' => [0, 0], '8' => [0, 1], '9' => [0, 2],
+  '4' => [1, 0], '5' => [1, 1], '6' => [1, 2],
+  '1' => [2, 0], '2' => [2, 1], '3' => [2, 2],
+                 '0' => [3, 1], 'A' => [3, 2]
 }
 
-BFS_CACHE = {}
-def bfs(start_val, dest_val, start_positions, dest_positions, grid)
-  cache_key = [start_val, dest_val, start_positions.object_id, dest_positions.object_id, grid.object_id]
-  return BFS_CACHE[cache_key] if BFS_CACHE.key?(cache_key)
+DIRECTIONAL_POSITIONS = {
+                 '^' => [0, 1], 'A' => [0, 2],
+  '<' => [1, 0], 'v' => [1, 1], '>' => [1, 2]
+}
 
-  start = start_positions[start_val]
-  dest = dest_positions[dest_val]
+GREEDY_CACHE = {}
+DEPTH_CACHE = {}
+
+def greedy_path(start_val, dest_val, positions, is_numeric_keypad)
+  cache_key = [start_val, dest_val, is_numeric_keypad]
+  return GREEDY_CACHE[cache_key] if GREEDY_CACHE.key?(cache_key)
+
+  start = positions[start_val]
+  dest = positions[dest_val]
 
   result = if start == dest
-    ["A"]
+    "A"
   else
-    queue = [start]
-    parent = { start => [] }
-    distance = { start => 0 }
-
-    head = 0
-    until queue.empty?
-      current = queue.shift
-
-      if distance.key?(dest) && distance[current] >= distance[dest]
-        next
-      end
-
-      DELTAS.each do |(di, dj), action|
-        ni, nj = current[0] + di, current[1] + dj
-        next unless ni.between?(0, grid.size - 1) && nj.between?(0, grid[0].size-1)
-        next if grid[ni][nj] == ' '
-
-        neighbour = [ni, nj]
-        new_dist = distance[current] + 1
-
-        if !distance.key?(neighbour)
-          distance[neighbour] = new_dist
-          parent[neighbour] = [[current, action]]
-          queue << neighbour
-        elsif distance[neighbour] == new_dist
-          parent[neighbour] << [current, action]
-        end
-      end
-    end
-
-    if !parent.key?(dest)
-      []
+    start_row, start_col = start
+    dest_row, dest_col = dest
+    
+    row_diff = dest_row - start_row
+    col_diff = dest_col - start_col
+    
+    updo = if row_diff > 0
+      'v' * row_diff
+    elsif row_diff < 0
+      '^' * (-row_diff)
     else
-      # Reconstruct all shortest paths
-      paths = []
-      q = [[dest, ""]] # [[node, path_str]]
-
-      until q.empty?
-        curr_node, curr_path_str = q.shift
-
-        if curr_node == start
-          paths << curr_path_str.reverse
-          next
-        end
-
-        if parent.key?(curr_node)
-          parent[curr_node].each do |prev, action|
-            q << [prev, curr_path_str + action]
-          end
-        end
+      ''
+    end
+    
+    leri = if col_diff > 0
+      '>' * col_diff
+    elsif col_diff < 0
+      '<' * (-col_diff)
+    else
+      ''
+    end
+    
+    if updo.empty?
+      leri + 'A'
+    elsif leri.empty?
+      updo + 'A'
+    else
+      path = if is_numeric_keypad
+        apply_numeric_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
+      else
+        apply_directional_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
       end
-
-      paths.map { |p| p + 'A' }
+      path + 'A'
     end
   end
 
-  BFS_CACHE[cache_key] = result
+  GREEDY_CACHE[cache_key] = result
   result
 end
 
-def apply(steps, pos_x, pos_y, grid)
+def get_sequence_length_at_depth(sequence, depth)
+  return sequence.length if depth == 0
+  
+  cache_key = [sequence, depth]
+  return DEPTH_CACHE[cache_key] if DEPTH_CACHE.key?(cache_key)
+  
+  sequence_with_start = "A" + sequence
+  pairs = sequence_with_start
+    .chars
+    .each_cons(2)
+    .map(&:join)
+  
+  total_length = 0
+  pairs.each do |pair|
+    path = greedy_path(pair[0], pair[1], DIRECTIONAL_POSITIONS, false)
+    total_length += get_sequence_length_at_depth(path, depth - 1)
+  end
+  
+  DEPTH_CACHE[cache_key] = total_length
+  total_length
+end
+
+def apply_numeric_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
+  # If you are on the bottom row (row 3) and going to the left column (col 0) -> updo + leri
+  if start_row == 3 && dest_col == 0
+    return updo + leri
+  end
+  
+  # If you are in the far-left column (col 0) and travelling to the bottom row (row 3) -> leri + updo
+  if start_col == 0 && dest_row == 3
+    return leri + updo
+  end
+  apply_general_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
+end
+
+def apply_directional_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
+  # If you are starting on the farthest left column (col 0, the "<" key) -> leri + updo
+  if start_col == 0
+    return leri + updo
+  end
+  
+  # If you are traveling to the farthest left column (col 0, the "<" key) -> updo + leri
+  if dest_col == 0
+    return updo + leri
+  end
+  apply_general_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
+end
+
+def apply_general_heuristics(start_row, start_col, dest_row, dest_col, updo, leri)
+  up_movement = dest_row < start_row  
+  left_movement = dest_col < start_col 
+  
+  case [up_movement, left_movement]
+  when [true, true]
+    leri + updo
+  when [false, true]
+    leri + updo
+  when [false, false]
+    updo + leri
+  when [true, false]
+    updo + leri
+  end
+end
+
+def get_initial_sequence(steps, positions, is_numeric_keypad)
   steps.prepend("A")
   pos = steps
     .chars
     .each_cons(2)
     .map(&:join)  
 
-  all = []
+  result = ""
   pos.each do |p|
-    all.push(bfs(p[0], p[1], pos_x, pos_y, grid))
+    result += greedy_path(p[0], p[1], positions, is_numeric_keypad)
   end
 
-  first, *rest = all
-  first.product(*rest).map(&:join)
+  result
 end
 
 sum = 0
 lines.each do |line|
-  result = apply(line, NUMERIC_POSITIONS, NUMERIC_POSITIONS, numeric_keypad)
-  2.times do
-    result = result.map do |each|
-      apply(each, DIRECTIONAL_POSITIONS, DIRECTIONAL_POSITIONS, directional_keypad)
-    end.flatten
-  end
-
-  shortest = result.min_by(&:length)
-  sum += (shortest.length * line[1..-2].to_i)
+  # First level: numeric keypad - get the initial sequence
+  result = get_initial_sequence(line, NUMERIC_POSITIONS, true)
+  
+  # Calculate the final length after 25 levels of directional keypads
+  total_length = get_sequence_length_at_depth(result, 25)
+  sum += (total_length * line[1..-2].to_i)
 end
 
 pp sum
-
-# pp bfs("A", "A", NUMERIC_POSITIONS, NUMERIC_POSITIONS, numeric_keypad)
-# pp bfs(">", ">", DIRECTIONAL_POSITIONS, DIRECTIONAL_POSITIONS, directional_keypad)
-# pp bfs(">", ">", DIRECTIONAL_POSITIONS, DIRECTIONAL_POSITIONS, directional_keypad)
-# pp bfs("A", "^", DIRECTIONAL_POSITIONS, DIRECTIONAL_POSITIONS, directional_keypad)
-# pp bfs("2", "9", NUMERIC_POSITIONS, NUMERIC_POSITIONS, numeric_keypad)
